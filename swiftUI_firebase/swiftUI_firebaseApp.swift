44ea12074d0c0e4efa84d55b746e1184b3bdb793
Henrique Alves Batochi <henrique.alvesbatochi@gmail.com>
2025-12-16 11:12:45 -0300

    Initial Commit

---
+++ swiftUI_firebase/swiftUI_firebaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftUI_firebaseApp.swift
+//  swiftUI_firebase
+//
+//  Created by Henrique Alves Batochi on 16/12/25.
+//
+
+import SwiftUI
+
+@main
+struct swiftUI_firebaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
